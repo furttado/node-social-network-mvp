57b74678bb1e1058ec80a94f578da572ab875b53
Furtado <furttado@gmail.com>
2020-07-19 12:44:50 -0300

    Routes added

---
+++ back/src/routes/PostRouter.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import { PostController } from '../controller/PostController'
+
+export const postRouter = express.Router()
+
+postRouter.post('/create', new PostController().createPost)
+postRouter.delete('/:id', new PostController().deletePost)
+postRouter.get('/user/:id', new PostController().getPostByUserId)
+postRouter.get('/all/:page', new PostController().getPostsAndNickname)
+postRouter.get('/feed', new PostController().getFeed)
---
+++ back/src/routes/UserRouter.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import { UserController } from '../controller/UserController'
+
+export const userRouter = express.Router();
+userRouter.post('/register', new UserController().register);
+userRouter.post('/login', new UserController().login);
+userRouter.post('/approve', new UserController().approve);
+userRouter.get('/:id', new UserController().getUserbyId);
+userRouter.post('/follow', new UserController().followUser)
+userRouter.delete('/unfollow', new UserController().unfollowUser)
